24011f54b25f5997c88c60d270762f1f27e523f6
Stefan Hahn <Stefan_Hahn1@gmx.net>
2017-06-09 01:07:44 +0200

    Added current state of winenv module

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='winenv',
+    version='0.0.0',
+
+    url='https://github.com/0xLeon/py-environment',
+    author='Stefan Hahn',
+    author_email='development@0xleon.com',
+
+    license='MIT',
+
+    packages=find_packages(),
+
+    install_requires=['future']
+)
