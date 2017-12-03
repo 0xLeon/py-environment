64ecab81bd2192645b75def184f59dadacd1cbaf
Stefan <Stefan_Hahn1@gmx.net>
2017-12-03 22:25:56 +0100

    Fix __future__ imports

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from setuptools import find_packages
 from setuptools import setup
 
--- winenv/__init__.py
+++ winenv/__init__.py
@@ -1,7 +1,7 @@
 """Provides function to edit environment variables in python"""
 
-from __future__ import unicode_literals
 from __future__ import absolute_import
+from __future__ import unicode_literals
 
 import os
 import ctypes
